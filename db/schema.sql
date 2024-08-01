f7b66b6596e08901f1a1ca6ce949555c12a69b38
unknown <brian.zhen101@gmail.com>
2024-08-01 00:18:48 -0400

    schema.sql created

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+
+\c employee_db;
+
+CREATE TABLE department (
+  id  SERIAL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department INTEGER,
+    FOREIGN KEY (department) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
