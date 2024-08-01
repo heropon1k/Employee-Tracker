422c1b07b8fa522e201d6d64b8766e38f2020517
unknown <brian.zhen101@gmail.com>
2024-08-01 00:23:45 -0400

    fixed schema

--- db/schema.sql
+++ db/schema.sql
@@ -6,22 +6,22 @@ CREATE DATABASE employee_db;
 
 CREATE TABLE department (
   id  SERIAL PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+  name VARCHAR(30) UNIQUE NOT NULL 
 );
 
 CREATE TABLE role (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department INTEGER,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department INTEGER NOT NULL,
     FOREIGN KEY (department) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INTEGER,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
     manager_id INTEGER,
     FOREIGN KEY (role_id) REFERENCES role(id),
     FOREIGN KEY (manager_id) REFERENCES employee(id)
