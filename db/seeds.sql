6df096c189eea4d713c47d0bd1e6992e0bb81f85
unknown <brian.zhen101@gmail.com>
2024-08-01 20:27:16 -0400

    add_roles now work

---
+++ db/seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO department (name)
+VALUES('Engineering'), 
+('Sales'), 
+('Finance'), 
+('Legal'), 
+('Marketing');
+
+INSERT INTO role (title, salary, department)
+VALUES('Engineer', 85000, 1), 
+('Senior Engineer', 125000, 1), 
+('Accountant', 350000, 3), 
+('Lawyer', 300000, 4);
+('Sales Lead', 10000,2),
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  
+ ('Jimmy', 'Morty', 2, null), 
+ ('Deon', 'Gibson', 1, 1), 
+ ('James', 'Smith', 1, 1),
+ ('Jamie', 'Brynlee', 1, 1),
+ ('Kevin', 'Carrol', 4, null);
+
--- index.js
+++ index.js
@@ -47,9 +47,11 @@ function main() {
                     break;
                 case 'View All Roles':
                     console.log('View All Roles');
+                    view_roles(pool);
                     break;
                 case 'Add Role':
                     console.log('Add Role');
+                    add_roles(pool);
                     break;
                 case 'View All Departments':
                     console.log('View All Departments');
@@ -70,54 +72,92 @@ function main() {
 
 main();
 
-function view_employee(pool){
-    pool.query('SELECT * FROM employee', (err, { rows })=> {
+function view_employee(pool) {
+    pool.query('SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department = department.id;', (err, { rows }) => {
         console.table(rows);
+        main();
     })
 }
 
-function add_employee(pool){
+function add_employee(pool) {
     inquirer
-    .prompt([
-        {
-            type: 'text',
-            name: 'first-name',
-            message: 'What is their first name?',
-        },
-        {
-            type: 'text',
-            name: 'last-name',
-            message: 'What is their last name?',
-        },
-    ])
-    .then((input) => {
-        const firstname = input.first-name;
-        const lastname = input.last-name;
+        .prompt([
+            {
+                type: 'text',
+                name: 'firstName',
+                message: 'What is their first name?',
+            },
+            {
+                type: 'text',
+                name: 'lastName',
+                message: 'What is their last name?',
+            },
+        ])
+        .then((input) => {
+            const firstname = input.firstName;
+            const lastname = input.lastName;
+            pool.query('SELECT role.title FROM department', (err, { rows }) => { })
 
 
-    })
+        })
 }
 
-function update_employee(){
-
-}
-
-function view_roles(){
+function update_employee() {
 
 }
 
-function add_roles(){
-
-}
-
-function view_department(){
-    pool.query('SELECT department.id, department.name FROM department', (err, { rows })=> {
+function view_roles() {
+    pool.query('SELECT * FROM role', (err, { rows }) => {
         console.table(rows);
-        main()
+        main();
     })
 }
 
-function add_department(pool){
+function add_roles() {
+    pool.query('SELECT * FROM department', (err, { rows }) => {
+        let departments = rows;
+        const department_db = departments.map(({id, name}) => ({
+            name: name,
+            value: id,
+        }));
+
+
+    
+    inquirer
+        .prompt([
+            {
+                type: 'text',
+                name: 'add_role',
+                message: 'What is the name of the role?',
+            },
+            {
+                type: 'number',
+                name: 'salary',
+                message: 'What is the salary?',
+            },
+            {
+                type: 'list',
+                name: 'department',
+                message: 'Which deparment does the role belong to?',
+                choices: department_db,
+            }
+        ])
+        .then((input) => {
+            pool.query(`INSERT INTO role (title, salary, department) VALUES ('${input.add_role}', ${input.salary}, '${input.department}')`)
+            console.log(`${input.add_role} role added`);
+        })
+        .then(() => main());
+    })
+}
+
+function view_department() {
+    pool.query('SELECT department.id, department.name FROM department', (err, { rows }) => {
+        console.table(rows);
+        main();
+    })
+}
+
+function add_department(pool) {
     inquirer
         .prompt([
             {
@@ -125,9 +165,9 @@ function add_department(pool){
                 name: 'add_department',
                 message: 'What is the name of the department?',
             }])
-            .then((input) => {
-                pool.query(`INSERT INTO department (name) VALUES ('${input.add_department}')`)
-                console.log(`${input.add_department} department added`)      
-            })
-            .then(() => main());
+        .then((input) => {
+            pool.query(`INSERT INTO department (name) VALUES ('${input.add_department}')`)
+            console.log(`${input.add_department} department added`)
+        })
+        .then(() => main());
 }
